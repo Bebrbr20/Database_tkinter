8045d1380d5c09968b608507e073d65810ffa9ca
bebrbr20 <bebrbr20@sps-prosek.cz>
2022-04-05 15:48:58 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import sqlite3
+import tkinter as tk
+
+# vytvoreni DB v paměti PC
+
+#con = sqlite3.connect(":memory:")
+
+con = sqlite3.connect("example.db")
+
+cur = con.cursor()
+cur.executescript("""
+ 
+    """)
+
+"""
+create table person(
+        jmeno,
+        prijmeni,
+        rodne_cislo,
+        pohlavi,
+        email,
+        telefon
+    );"""
+
+
+
+data = cur.execute("SELECT * FROM person ORDER BY jmeno")
+
+table = []
+
+for row in data:
+    print(row)
+    table.append(row)
+con.close()
+
+
+
+# Hlavní konfigurace tkinteru
+window = tk.Tk()
+window.geometry("570x300")
+window.title("Databáze")
+
+label1=tk.Label(text = "Databáze uživatelů")
+label1.pack()
+
+
+
+
+window.mainloop()
