d53951cec5337ae1afa0d3cb142c643d5ba435f6
Bruno Bébr <letaxcz@gmail.com>
2022-04-19 07:12:37 +0200

    uprava

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlite3
 import tkinter as tk
+import time
 
 # vytvoreni DB v paměti PC
 
@@ -40,24 +41,44 @@ def New_User():
     create.pack_forget()
     ljmeno=tk.Label(text="Jméno")
     jmeno=tk.Entry(text="Jméno")
-    prijmeni = tk.Entry(text="Jméno")
-    rodne_cislo = tk.Entry(text="Jméno")
-    pohlavi = tk.Entry(text="Jméno")
-    email = tk.Entry(text="Jméno")
-    telefon = tk.Entry(text="Jméno")
+    lprijmeni = tk.Label(text="Příjmení")
+    prijmeni = tk.Entry()
+    lrodne_cislo = tk.Label(text="Rodné číslo")
+    rodne_cislo = tk.Entry()
+    lpohlavi = tk.Label(text="Pohlaví")
+    pohlavi = tk.Entry()
+    lemail = tk.Label(text="Email")
+    email = tk.Entry()
+    ltelefon = tk.Label(text="Telefonní číslo")
+    telefon = tk.Entry()
 
     ljmeno.pack()
     jmeno.pack()
+    lprijmeni.pack()
     prijmeni.pack()
+    lrodne_cislo.pack()
     rodne_cislo.pack()
+    lpohlavi.pack()
     pohlavi.pack()
+    lemail.pack()
     email.pack()
+    ltelefon.pack()
     telefon.pack()
+
+
+    submit = tk.Button(text="Přidat", command=save_user)
+    submit.pack()
+
+
+def save_user():
+    if jmeno.get() != "" and prijmeni.get() != "" and rodne_cislo.get() != "" and pohlavi.get() != "" and email.get() != "" and telefon.get() != "":
+        print("aaa")
+
 print(table)
 
 # Hlavní konfigurace tkinteru
 window = tk.Tk()
-window.geometry("570x300")
+window.geometry("570x350")
 window.title("Databáze")
 
 label1=tk.Label(text = "Databáze uživatelů")
