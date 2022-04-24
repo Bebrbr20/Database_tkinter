00dac540612c2fe18376284a8cd4c1b408cfe519
Bruno Bébr <letaxcz@gmail.com>
2022-04-24 12:19:35 +0200

    uprava

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sqlite3
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
-
+import  datetime
 
 window = tk.Tk()
 window.geometry("900x250")
@@ -35,31 +35,131 @@ cur.executescript("""
 """
 
 
-
-
-
+def validaceRC(rc):
+    x = list(rc)
+    if len(rc) == 11:
+        if x[6] == '/':
+            count = x[0]+x[1]+x[2]+x[3]+x[4]+x[5]+x[7]+x[8]+x[9]+x[10]
+            if int(count)%11 == 0:
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
 
 def save_user(jmeno, prijmeni, rodne_cislo, pohlavi, email, telefon):
-    if jmeno != "" and prijmeni != "" and rodne_cislo != "" and pohlavi != "" and email != "" and telefon != "":
-        con = sqlite3.connect("example.db")
-
-        cur = con.cursor()
-
-        cur.executescript("""
-             insert into person(jmeno,prijmeni,rodne_cislo,pohlavi,email,telefon)
-                 values (
-                    %jmeno,
-                    %prijmeni,
-                    %rodne_cislo,
-                    %pohlavi,
-                    %email,
-                    %telefon
-                    )
-            """)
+    if jmeno.get() == '':
+        jmeno.config(bg='red')
     else:
-        errorLabel=tk.Label( text="ERROR")
-        errorLabel.grid(row=1, column= 5)
+        jmeno.config(bg='white')
+    if prijmeni.get() == '':
+        prijmeni.config(bg='red')
+    else:
+        prijmeni.config(bg='white')
+    if rodne_cislo.get() == '' or not validaceRC(rodne_cislo.get()):
+        rodne_cislo.config(bg='red')
+    else:
+        rodne_cislo.config(bg='white')
+    if pohlavi.get() == '':
+        pohlavi.config(bg='red')
+    else:
+        pohlavi.config(bg='white')
+    if email.get() == '':
+        email.config(bg='red')
+    else:
+        email.config(bg='white')
+    if telefon.get() == '':
+        telefon.config(bg='red')
+    else:
+        telefon.config(bg='white')
 
+    if jmeno.get() != '' or prijmeni.get() != '' or rodne_cislo.get() != '' or pohlavi.get() != '' or email.get() != '' or telefon.get() != '':
+        if not validaceRC(rodne_cislo.get()):
+            try:
+
+                con = sqlite3.connect("example.db")
+
+                cur = con.cursor()
+                cur.execute("insert into person(jmeno,prijmeni,rodne_cislo,pohlavi,email,telefon) values(?,?,?,?,?,?)",(jmeno.get(), prijmeni.get(), rodne_cislo.get(), pohlavi.get(), email.get(), telefon.get()))
+                con.commit()
+                tv.insert("", tk.END, values=(jmeno, prijmeni, rodne_cislo, pohlavi, email, telefon))
+                return "OK"
+            except:
+                print("ERROR PŘI ZAPISOVÁNÍ")
+                return "ERROR PŘI ZAPISOVÁNÍ"
+
+
+
+def select_item(event):
+    global selected_item
+
+    rowid = tv.identify_row(event.y)
+    selected_item = tv.item(tv.focus())
+
+    print (selected_item['values'])
+
+
+    edituserframe = tk.Frame(window, width=900, height=250)
+
+    edituserframe.grid(row=0, column=0)
+    edituserframe.grid_propagate(0)
+
+    ljmeno=tk.Label(edituserframe,text="Jméno")
+    ljmeno.grid(column=4, row=1)
+
+    jmeno=tk.Entry(edituserframe,text="Jméno")
+    jmeno.grid(column=5, row=1)
+
+    lprijmeni = tk.Label(edituserframe,text="Příjmení")
+    lprijmeni.grid(column=4, row=2)
+
+    prijmeni = tk.Entry(edituserframe)
+    prijmeni.grid(column=5, row=2)
+
+    lrodne_cislo = tk.Label(edituserframe,text="Rodné číslo")
+    lrodne_cislo.grid(column=4, row=3)
+
+    rodne_cislo = tk.Entry(edituserframe)
+    rodne_cislo.grid(column=5, row=3)
+
+    lpohlavi = tk.Label(edituserframe,text="Pohlaví")
+    lpohlavi.grid(column=4, row=4)
+
+    pohlavi = tk.Entry(edituserframe)
+    pohlavi.grid(column=5, row=4)
+
+    lemail = tk.Label(edituserframe,text="Email")
+    lemail.grid(column=4, row=5)
+
+    email = tk.Entry(edituserframe)
+    email.grid(column=5, row=5)
+
+    ltelefon = tk.Label(edituserframe,text="Telefonní číslo")
+    ltelefon.grid(column=4, row=6)
+
+    telefon = tk.Entry(edituserframe)
+    telefon.grid(column=5, row=6)
+
+    jmeno.insert(tk.END, selected_item['values'][0])
+    prijmeni.insert(tk.END, selected_item['values'][1])
+    rodne_cislo.insert(tk.END, selected_item['values'][2])
+    pohlavi.insert(tk.END, selected_item['values'][3])
+    email.insert(tk.END, selected_item['values'][4])
+    telefon.insert(tk.END, selected_item['values'][5])
+
+    submit = tk.Button(
+        edituserframe,
+        text="Přidat",
+        command=lambda: save_user(jmeno,prijmeni,rodne_cislo, pohlavi, email, telefon))
+    submit.grid(column=0, row=9, sticky='w')
+
+    backbutton = tk.Button(edituserframe, text="Zpět", fg="black", command=lambda:edituserframe.grid_forget())
+    backbutton.grid(row=9, column=1, sticky='w')
+
+    exitbutton = tk.Button(edituserframe, text="Ukončit aplikaci", fg="black", command=exit)
+    exitbutton.grid(row=9, column=2, sticky='w')
 def exit():
     window.destroy()
 
@@ -112,7 +212,7 @@ def New_User():
     submit = tk.Button(
         newuserframe,
         text="Přidat",
-        command=lambda: save_user(jmeno.get(),prijmeni.get(),rodne_cislo.get(), pohlavi.get(), email.get(), telefon.get()))
+        command=lambda: save_user(jmeno,prijmeni,rodne_cislo, pohlavi, email, telefon))
     submit.grid(column=0, row=9, sticky='w')
 
     backbutton = tk.Button(newuserframe, text="Zpět", fg="black", command=lambda:newuserframe.grid_forget())
@@ -155,14 +255,14 @@ data = cur.execute("SELECT * FROM person ORDER BY jmeno")
 rows = data.fetchall()
 
 for row in rows:
-    print(row)
-
     tv.insert("", tk.END, values=row)
 
 data.close()
 con.close()
 
+tv.bind('<Double-1>', select_item)
 tv.pack()
+
 
 
 bluebutton = tk.Button(mainframe, text="Přidání uživatele", fg="black" , command=New_User)
@@ -175,6 +275,7 @@ blackbutton = tk.Button(mainframe, text="Ukončit aplikaci", fg="black", command
 blackbutton.pack(side=tk.LEFT)
 
 mainframe.grid(row=0, column=0)
+
 
 
 window.mainloop()
